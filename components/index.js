2cafc04b227cb39c31297c8bf861640541049a74
Mogakamo <mogaka.amo254@gmail.com>
2022-06-18 20:24:40 +0300

    Created the specific components and added them to the index page

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as NavBar } from "./NavBar";
+export { default as Footer } from "./Footer";
+export {default as Blog} from "./Blog";
+export {default as CallToAction} from "./CallToAction";
+export {default as Hero } from "./Hero";
+export {default as Services} from "./Services";
