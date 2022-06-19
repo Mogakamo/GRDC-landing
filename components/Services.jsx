183acca26991fc868dff30485c38b775be4f5b78
Mogakamo <mogaka.amo254@gmail.com>
2022-06-19 16:28:34 +0300

    commit

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ServiceCard from './Reusables/ServiceCard'
 
-
+const service = [
+  {
+    
+  },
+  {},
+  {}
+]
 
 const Services = () => {
   return (
     <div className=' relative text-center p-10'>
       <h1 className='font-bold text-2xl pb-16'>Discover the Power of the GRDC Platform</h1>
       <div className='space-x-10'>
+      <ServiceCard />
+      <ServiceCard />
       <ServiceCard />
       </div>
     </div>
