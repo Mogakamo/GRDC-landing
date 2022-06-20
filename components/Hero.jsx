d775203c4341cb18919a1a3808111d7d3299cfcb
Mogakamo <mogaka.amo254@gmail.com>
2022-06-20 14:42:52 +0300

    starting the day

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -30,8 +30,8 @@ const Hero = () => {
                       text-white flex items-center px-4 hover:bg-[#0E7506]"
           >
             <div className="flex items-center space-x-3 px-2">
-            <Link href="/">Our Services</Link>
-            <AiOutlineArrowRight />
+              <Link href="/">Our Services</Link>
+              <AiOutlineArrowRight />
             </div>
           </button>
         </div>
@@ -46,8 +46,70 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div>1</div>
-      <div>1</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 p-10 h-[90vh]">
+        <div className="flex flex-col items-start justify-center space-y-10">
+          <h1 className="text-[#0E7506] font-bold text-2xl">
+            Fractional Investements
+          </h1>
+          <p className="text-white text-start">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit
+            amet luctus venenatis, lectus magna fringilla
+          </p>
+          <button
+            className="border-[#0E7506] p-2 border-2 rounded-full 
+                      text-white flex items-center px-4 hover:bg-[#0E7506]"
+          >
+            <div className="flex items-center space-x-3 px-2">
+              <Link href="/">Learn More</Link>
+              <AiOutlineArrowRight />
+            </div>
+          </button>
+        </div>
+        <div className="p-10">
+          <div className="h-48 -z-10 relative">
+            <Image src="/hero.svg" layout="fill" />
+            <div className="pt-10">
+              <div className="border-[#0E7506] bg-white rounded-3xl z-50 h-48 w-96">
+                Hello
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 p-10 h-[90vh]">
+        <div className="flex flex-col items-start justify-center space-y-10">
+          <h1 className="text-[#0E7506] font-bold text-2xl">
+            GRDC the future
+          </h1>
+          <p className="text-white text-start">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit
+            amet luctus venenatis, lectus magna fringilla
+          </p>
+          <button
+            className="border-[#0E7506] p-2 border-2 rounded-full 
+                      text-white flex items-center px-4 hover:bg-[#0E7506]"
+          >
+            <div className="flex items-center space-x-3 px-2">
+              <Link href="/">Contact Us</Link>
+              <AiOutlineArrowRight />
+            </div>
+          </button>
+        </div>
+        <div className="p-10">
+          <div className="h-48 -z-10 relative">
+            <Image src="/hero.svg" layout="fill" />
+            <div className="pt-10">
+              <div className="border-[#0E7506] bg-white rounded-3xl z-50 h-48 w-96">
+                Hello
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Carousel>
   );
 };
