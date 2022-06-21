be95a3c5aca5f290e1dc717bbca1224ec8c99fbf
Mogakamo <mogaka.amo254@gmail.com>
2022-06-21 10:22:04 +0300

    commit

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -5,7 +5,7 @@ const Blog = () => {
   return (
     <div className="bg-[#0E7506] bg-opacity-10 p-20">
       <h1 className="pb-20 font-semibold text-2xl text-center">Learn more about GRDC</h1>
-      <div className="flex  space-x-10">
+      <div className="md:flex  md:space-x-10 space-y-10 md:space-y-0">
         <BlogCard />
         <BlogCard />
         <BlogCard />
