52315dbacb62a2d1c988c3d55d89826ea79ca939
Mogakamo <mogaka.amo254@gmail.com>
2022-06-21 10:14:40 +0300

    hero section is responsive

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,12 +12,12 @@ const Hero = () => {
       showArrows={false}
       // showIndicators={false}
       showStatus={false}
-      interval={3000}
-      autoPlay
-      infiniteLoop={true}
+      // interval
+      // autoPlay
+      // infiniteLoop
     >
-      <div className="flex p-10 h-[90vh] items-center justify-center space-x-20 px-20 mx-auto">
-        <div className="flex flex-col items-start w-[450px] space-y-10">
+      <div className="grid grid-cols-1 place-content-center place-items-center md:flex p-10 h-[90vh] items-center justify-center space-x-20 px-20 mx-auto">
+        <div className=" md:flex md:flex-col flex-col items-center md:items-start w-[450px] space-y-10">
           <h1 className="text-[#0E7506] font-semibold text-2xl">
             Fractional Investements
           </h1>
@@ -55,8 +55,8 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="flex p-10 h-[90vh] items-center justify-center space-x-20 px-20 mx-auto">
-        <div className="flex flex-col items-start w-[450px] space-y-10">
+      <div className="grid grid-cols-1 place-content-center place-items-center md:flex p-10 h-[90vh] items-center justify-center space-x-20 px-20 mx-auto">
+        <div className="flex flex-col md:items-start items-center w-[450px] space-y-10">
           <h1 className="text-[#0E7506] font-semibold text-2xl">
             Fractional Investements
           </h1>
@@ -94,8 +94,8 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="flex p-10 h-[90vh] items-center justify-center space-x-20 px-20 mx-auto">
-        <div className="flex flex-col items-start w-[450px] space-y-10">
+      <div className="grid grid-cols-1 place-content-center place-items-center md:flex p-10 h-[90vh] items-center justify-center space-x-20 px-20 mx-autoo">
+        <div className="flex flex-col md:items-start items-center w-[450px] space-y-10">
           <h1 className="text-[#0E7506] font-semibold text-2xl">
             Fractional Investements
           </h1>
