4a2c3a2244783a7e12d5b803a5f166fec09588bf
Mogakamo <mogaka.amo254@gmail.com>
2022-06-21 09:15:08 +0300

    adjusted the blog section

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -3,8 +3,8 @@ import BlogCard from "./Reusables/BlogCard";
 
 const Blog = () => {
   return (
-    <div className="bg-[#0E7506] bg-opacity-10 p-10">
-      <h1 className="pb-6 font-semibold text-2xl text-center">Learn more about GRDC</h1>
+    <div className="bg-[#0E7506] bg-opacity-10 p-20">
+      <h1 className="pb-20 font-semibold text-2xl text-center">Learn more about GRDC</h1>
       <div className="flex  space-x-10">
         <BlogCard />
         <BlogCard />
