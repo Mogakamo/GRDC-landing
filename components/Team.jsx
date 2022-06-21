74f86b2b3b4f6bcd605b499a62e933395890e3e2
Mogakamo <mogaka.amo254@gmail.com>
2022-06-21 05:08:17 +0300

    Finished with the Team

--- components/Team.jsx
+++ components/Team.jsx
@@ -3,11 +3,22 @@ import React from "react";
 
 const Team = () => {
   return (
-    <div className="h-96 flex items-center p-10 flex-col">
+    <div className="h-auto flex items-center p-10 flex-col">
       <h1>Our Team</h1>
       <p>This is our team</p>
-      <div className="relative h-20">
-        <Image src="/ceo.svg" layout="fill" />
+      <div className="flex items-center">
+      <div className="relative h-96 w-96 ">
+        <Image src="/ceo.svg" layout="fill" objectFit="cover" />
+      </div>
+      <div className="relative h-72 w-48">
+        <Image src="/sales.svg" layout="fill" objectFit="cover" />
+      </div>
+      <div className="relative h-72 w-48">
+        <Image src="/design.svg" layout="fill" objectFit="cover" />
+      </div>
+      <div className="relative h-72 w-48">
+        <Image src="/ux.svg" layout="fill" objectFit="cover" />
+      </div>
       </div>
     </div>
   );
