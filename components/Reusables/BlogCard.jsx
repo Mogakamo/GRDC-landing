fa8069f4d874bb8844e6e657aa65933c08d505ba
Mogakamo <mogaka.amo254@gmail.com>
2022-06-21 07:09:13 +0300

    finished on the blog section

--- components/Reusables/BlogCard.jsx
+++ components/Reusables/BlogCard.jsx
@@ -3,10 +3,11 @@ import React from "react";
 
 const BlogCard = () => {
   return (
-    <div className="flex flex-col items-start space-y-2 p-4 w-auto h-auto border-transparent bg-white rounded-xl shadow-lg mx">
+    <div className="flex flex-col items-start space-y-2 p-4 w-auto h-auto border-transparent 
+        bg-white rounded-xl shadow-lg transition-transform duration-300 hover:scale-125 ease-in-out overflow-auto">
       <h1 className="font-bold">Episode 3 - 15% drop in Detroit: Opportunity or threat?</h1>
       <h2 className="text-[#0E7506]">May 22, 2022</h2>
-      <p>
+      <p className="font-extralight text-sm">
         15% drop in Detroit: Opportunity or threat? Vous pouvez retrouver le
         version française de cet article en cliquant ici. For some time now, it
         has been possible to find articles or tweets indicating that real estate
