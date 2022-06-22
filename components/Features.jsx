dc019b9879e4b80d3401986b2f0802cfe997b0b0
Mogakamo <mogaka.amo254@gmail.com>
2022-06-22 22:10:02 +0300

    finished with the features

--- components/Features.jsx
+++ components/Features.jsx
@@ -1,47 +1,63 @@
+import Image from "next/image";
 import React from "react";
+import { MdGeneratingTokens } from "react-icons/md";
 
 const Features = () => {
   return (
-    <div className="h-96 mx-32">
-      <div className="intro-desc">
-        <div className="inner">
-          {[
-            {
-              icon: icons.token,
-              title: "Unique Tokens",
-              content:
-                "Ownership of each property is distributed across a finite number of representative tokens. Based on token share, owners can collect revenue from rent, and vote on property decisions.",
-            },
-            {
-              icon: icons.property,
-              title: "Property Managed",
-              content:
-                "Each GRDC property has a property management company managing the property on RealToken owners behalf. The property management company sources tenants, collects rent, and manages repairs, so the diverse group of GRDC owners don’t have to.",
-            },
-            {
-              icon: icons.owned,
-              title: "LLC Owned",
-              content:
-                "Real estate can’t directly be tokenized, but legal entities can. Each property is owned by company (either an Inc or an LLC). Each company is tokenized as a unique set of GRDC and made available for purchase. ",
-            },
-            {
-              icon: icons.money,
-              title: "Instant Payments",
-              content:
-                "With blockchains, we no longer need to wait 30 days to receive a bank transfer. Owning property with GRDC allows you to get and monitor you investment real-time. Dividends are paid using a US-Dollar stablecoin, sent to your wallet.",
-            },
-          ].map((each, i) => (
-            <div className="each-desc" key={i}>
-              <span
-                className="icon"
-                dangerouslySetInnerHTML={{ __html: each.icon }}
-              ></span>
-              <div className="content">
-                <h1>{each.title}</h1>
-                <p>{each.content}</p>
-              </div>
-            </div>
-          ))}
+    <div className="h-auto mx-32 p-20">
+      <div className="grid gap-12 grid-cols-2">
+        {" "}
+        <div className="flex flex-col">
+          <div className="flex items-center justify-start space-x-3">
+            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
+            <h1 className="text-[#061b05] text-xl font-semibold">
+              Unique Tokens
+            </h1>
+          </div>
+          <p className="leading-loose font-light">
+            Ownership of each property is distributed across a finite number of
+            representative tokens. Based on token share, owners can collect
+            revenue from rent, and vote on property decisions.
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex items-center justify-start space-x-3">
+            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
+            <h1 className="text-[#061b05] text-xl font-semibold">
+              Unique Tokens
+            </h1>
+          </div>
+          <p className="leading-loose font-light">
+            Ownership of each property is distributed across a finite number of
+            representative tokens. Based on token share, owners can collect
+            revenue from rent, and vote on property decisions.
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex items-center justify-start space-x-3">
+            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
+            <h1 className="text-[#061b05] text-xl font-semibold">
+              Unique Tokens
+            </h1>
+          </div>
+          <p className="leading-loose font-light">
+            Ownership of each property is distributed across a finite number of
+            representative tokens. Based on token share, owners can collect
+            revenue from rent, and vote on property decisions.
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex items-center justify-start space-x-3">
+            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
+            <h1 className="text-[#061b05] text-xl font-semibold">
+              Unique Tokens
+            </h1>
+          </div>
+          <p className="leading-loose font-light">
+            Ownership of each property is distributed across a finite number of
+            representative tokens. Based on token share, owners can collect
+            revenue from rent, and vote on property decisions.
+          </p>
         </div>
       </div>
     </div>
