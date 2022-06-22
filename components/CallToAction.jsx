46b77f4c4cd6e82c069a2cd8afad006add89791d
Mogakamo <mogaka.amo254@gmail.com>
2022-06-22 21:34:39 +0300

    call to action

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 const CallToAction = () => {
   return (
-    <div className="w-full">
-      <div className="h-96 md:grid md:grid-cols-2">
-        <div className="flex items-center justify-center">
+    <div className="w-full pt-10">
+      <div className="h-auto md:grid md:grid-cols-2 bg-[#0E7506] bg-opacity-10 py-10 pb-10">
+        <div className="flex ml-32 justify-center">
           <div className="relative h-72 w-48">
             <Image src="/house.jpg" layout="fill" objectFit="cover" />
           </div>
