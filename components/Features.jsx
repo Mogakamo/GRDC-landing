7ac043453dcb09b43f09a416f84fa4699c6b2fc1
Mogakamo <mogaka.amo254@gmail.com>
2022-06-23 05:11:33 +0300

    finisfinished with the features section

--- components/Features.jsx
+++ components/Features.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import { MdGeneratingTokens } from "react-icons/md";
+import { GiHouse } from "react-icons/gi";
+import { FaRegMoneyBillAlt } from "react-icons/fa";
+import { BsShieldFillCheck } from "react-icons/bs";
 
 const Features = () => {
   return (
@@ -9,7 +12,7 @@ const Features = () => {
         {" "}
         <div className="flex flex-col">
           <div className="flex items-center justify-start space-x-3">
-            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
+            <MdGeneratingTokens className="h-10 w-10 text-[#0E7506]" />
             <h1 className="text-[#061b05] text-xl font-semibold">
               Unique Tokens
             </h1>
@@ -21,42 +24,43 @@ const Features = () => {
           </p>
         </div>
         <div className="flex flex-col">
-          <div className="flex items-center justify-start space-x-3">
-            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
+          <div className="flex items-center justify-items-end space-x-3">
+            <GiHouse className="h-8 w-8 text-[#0E7506]" />
             <h1 className="text-[#061b05] text-xl font-semibold">
-              Unique Tokens
+              Property Managed
             </h1>
           </div>
-          <p className="leading-loose font-light">
-            Ownership of each property is distributed across a finite number of
-            representative tokens. Based on token share, owners can collect
-            revenue from rent, and vote on property decisions.
+          <p className="leading-loose font-light w-96">
+            Each GRDC property has a property management company managing the
+            property on RealToken owners behalf. The property management company
+            sources tenants, collects rent, and manages repairs, so the diverse
+            group of GRDC owners don't have to.
           </p>
         </div>
         <div className="flex flex-col">
           <div className="flex items-center justify-start space-x-3">
-            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
-            <h1 className="text-[#061b05] text-xl font-semibold">
-              Unique Tokens
-            </h1>
+            <BsShieldFillCheck className="h-8 w-8 text-[#0E7506]" />
+            <h1 className="text-[#061b05] text-xl font-semibold">LLC Owned</h1>
           </div>
-          <p className="leading-loose font-light">
-            Ownership of each property is distributed across a finite number of
-            representative tokens. Based on token share, owners can collect
-            revenue from rent, and vote on property decisions.
+          <p className="leading-loose font-light text-center">
+            Real estate can't directly be tokenised, but legal entities can.
+            Each property is owned by company(either an Inc or an LLC). Each
+            company is tokenised as a unique set of GRDC and made available for
+            purchase.
           </p>
         </div>
         <div className="flex flex-col">
           <div className="flex items-center justify-start space-x-3">
-            <MdGeneratingTokens className="h-16 w-16 text-[#0E7506]" />
+            <FaRegMoneyBillAlt className="h-8 w-8 text-[#0E7506]" />
             <h1 className="text-[#061b05] text-xl font-semibold">
-              Unique Tokens
+              Instant Payments
             </h1>
           </div>
-          <p className="leading-loose font-light">
-            Ownership of each property is distributed across a finite number of
-            representative tokens. Based on token share, owners can collect
-            revenue from rent, and vote on property decisions.
+          <p className="leading-loose font-light text-cente w-96">
+            With blockchains, we no longer ned to wait 30 days to receive a bank
+            transfer. Owning property with GRDC allows you to get and monitor
+            your investments in real-time. Dividends are paid using a US-Dollar
+            stablecoin, sent to your wallet.
           </p>
         </div>
       </div>
